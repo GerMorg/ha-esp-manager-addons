b20aefc21590dc6a5ab40de4020e9e0fd7cd7d7e
GerMorg <71175126+GerMorg@users.noreply.github.com>
2026-08-14 19:37:56 +0200

    wlan fix

---
+++ esp_manager_dev/app/templates/src/main.cpp
@@ -0,0 +1,5 @@
+#include <Arduino.h>
+#include <ESPManager.h>
+extern void setupDevice(); extern void loopDevice();
+void setup(){ESPManager.begin();setupDevice();}
+void loop(){ESPManager.loop();loopDevice();}
